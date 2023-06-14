1c6d40f86fdf2deccc344d4416cd22acd3dc13b8
yu-uichi <86936477+yu-uichi@users.noreply.github.com>
2023-06-14 20:25:08 +0900

    Add files via upload

--- config.php
+++ config.php
@@ -1,19 +1,19 @@
 <?php
 
     //サーバのデータベース
-    // define('DSN', 'mysql:host=mysql2105.xserver.jp;dbname=teamdigicom_nrdb');
-    // define('DB_USER', 'teamdigicom_nr');
-    // // define('DB_USER', 'teamdigicom_krt');
-    // define('DB_PASSWORD', 'Xaet2yie');
+    define('DSN', 'mysql:host=mysql2105.xserver.jp;dbname=teamdigicom_nede');
+    // define('DB_USER', 'teamdigicom_nede');
+    define('DB_USER', 'teamdigicom_nr');
+    define('DB_PASSWORD', 'V5pWJC6e');
 
     // $APP_TITLE_EXPLAIN = "模擬電子カルテ実習記録学習サイト";
     // $APP_TITLE = "N-Repo";
 
 
     //localhostのデータベース
-    define('DSN', 'mysql:host=localhost;dbname=teamdigicom_mixdb');
-    define('DB_USER', 'teamdigicom_mix');
-    define('DB_PASSWORD', 'Yk3fxzr4');
+    //define('DSN', 'mysql:host=localhost;dbname=teamdigicom_mixdb');
+    //define('DB_USER', 'teamdigicom_mix');
+    //define('DB_PASSWORD', 'Yk3fxzr4');
 
     $APP_TITLE_EXPLAIN = "模擬電子カルテ実習記録学習サイト";
     $APP_TITLE = "アプリ名";
